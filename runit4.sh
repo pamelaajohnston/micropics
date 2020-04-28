a80350bcfb088ad6c223302f89443de697549661
Pam Johnston <1609098@rgu.ac.uk>
2020-04-28 17:28:09 +0100

    typo

--- runit4.sh
+++ runit4.sh
@@ -54,7 +54,7 @@ echo $(date) >> ${resultsFile}
 
 echo "Started at:" >> ${resultsFile}
 echo $(date) >> ${resultsFile}
-nohup python3 processImages.py -a /home/pam/data/micropics/after -b /home/pam/data/micropics/before -d /home/pam/data/micropics/workingSet_morphed -f 4 -g 0 -m 4 -o sgd -e 10 >> ${resultsFile}
+nohup python3 processImages.py -a /home/pam/data/micropics/after -b /home/pam/data/micropics/before -d /home/pam/data/micropics/workingSet_morphed -f 0 -g 0 -m 4 -o sgd -e 10 >> ${resultsFile}
 echo "Finished/killed at:" >> ${resultsFile}
 echo $(date) >> ${resultsFile}
 
