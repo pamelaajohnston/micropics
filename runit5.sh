a012a5369b2a74cb5fac652f70717ab4fee74309
Pam Johnston <1609098@rgu.ac.uk>
2020-04-29 09:14:07 +0100

    typo maybe

--- runit5.sh
+++ runit5.sh
@@ -3,7 +3,7 @@ echo "Starting the test"
 
 resultsFile="test.txt"
 
-workingDir = "/home/pam/data/micropics/workingSet_hpFiltered"
+workingDir="/home/pam/data/micropics/workingSet_hpFiltered"
 echo "Started at:" >> ${resultsFile}
 echo $(date) >> ${resultsFile}
 nohup python3 processImages.py -a /home/pam/data/micropics/after -b /home/pam/data/micropics/before -d ${workingDir} -f 0 -g 0 -m 4 -o sgd -e 30 >> ${resultsFile}
@@ -56,24 +56,24 @@ echo $(date) >> ${resultsFile}
 workingDir = "/home/pam/data/micropics/workingSet_hpFiltered"
 echo "Started at:" >> ${resultsFile}
 echo $(date) >> ${resultsFile}
-nohup python3 processImages.py -a /home/pam/data/micropics/after -b /home/pam/data/micropics/before -d ${workingDir} -f 0 -g 0 -m 5 -o sgd -e 10 >> ${resultsFile}
+nohup python3 processImages.py -a /home/pam/data/micropics/after -b /home/pam/data/micropics/before -d ${workingDir} -f 0 -g 0 -m 6 -o sgd -e 10 >> ${resultsFile}
 echo "Finished/killed at:" >> ${resultsFile}
 echo $(date) >> ${resultsFile}
 
 echo "Started at:" >> ${resultsFile}
 echo $(date) >> ${resultsFile}
-nohup python3 processImages.py -a /home/pam/data/micropics/after -b /home/pam/data/micropics/before -d ${workingDir} -f 0 -g 0 -m 5 -o adam -e 10 >> ${resultsFile}
+nohup python3 processImages.py -a /home/pam/data/micropics/after -b /home/pam/data/micropics/before -d ${workingDir} -f 0 -g 0 -m 6 -o adam -e 10 >> ${resultsFile}
 echo "Finished/killed at:" >> ${resultsFile}
 echo $(date) >> ${resultsFile}
 
 echo "Started at:" >> ${resultsFile}
 echo $(date) >> ${resultsFile}
-nohup python3 processImages.py -a /home/pam/data/micropics/after -b /home/pam/data/micropics/before -d ${workingDir} -f 0 -g 0 -m 5 -o rmsprop -e 10 >> ${resultsFile}
+nohup python3 processImages.py -a /home/pam/data/micropics/after -b /home/pam/data/micropics/before -d ${workingDir} -f 0 -g 0 -m 6 -o rmsprop -e 10 >> ${resultsFile}
 echo "Finished/killed at:" >> ${resultsFile}
 echo $(date) >> ${resultsFile}
 
 echo "Started at:" >> ${resultsFile}
 echo $(date) >> ${resultsFile}
-nohup python3 processImages.py -a /home/pam/data/micropics/after -b /home/pam/data/micropics/before -d ${workingDir} -f 0 -g 0 -m 5 -o adadelta -e 10 >> ${resultsFile}
+nohup python3 processImages.py -a /home/pam/data/micropics/after -b /home/pam/data/micropics/before -d ${workingDir} -f 0 -g 0 -m 6 -o adadelta -e 10 >> ${resultsFile}
 echo "Finished/killed at:" >> ${resultsFile}
 echo $(date) >> ${resultsFile}
