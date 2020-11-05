bdd62127eb88047583b359e7b387aeb8a1b5128e
Pam Johnston <1609098@rgu.ac.uk>
2020-11-05 17:13:46 +0000

    taking out what isn't needed

--- runGANs2.sh
+++ runGANs2.sh
@@ -1,7 +1,5 @@
 CUDA_VISIBLE_DEVICES=7 python cycleGANKeras.py -s aphaniz_rd_ori1.npz -d aph_cgan2_rdToOri > aph_cgan2_rdtooriout.txt
 CUDA_VISIBLE_DEVICES=7 python cycleGANKeras.py -s aphaniz_ori_lab1.npz -d aph_cgan2_oriToLab > aph_cgan2_oritolabout.txt
-CUDA_VISIBLE_DEVICES=7 python cycleGANKeras.py -s aphaniz_ori_rd1.npz -d aph_cgan2_oriToRD > aph_cgan2_oritordout.txt
 
 CUDA_VISIBLE_DEVICES=7 python cycleGANKeras.py -s aphaniz_rd_ori2.npz -d aph_cgan2_rdToOri2 > aph_cgan2_rdtooriout2.txt
 CUDA_VISIBLE_DEVICES=7 python cycleGANKeras.py -s aphaniz_ori_lab2.npz -d aph_cgan2_oriToLab2 > aph_cgan2_oritolabout2.txt
-CUDA_VISIBLE_DEVICES=7 python cycleGANKeras.py -s aphaniz_ori_rd2.npz -d aph_cgan2_oriToRD2 > aph_cgan2_oritordout2.txt
