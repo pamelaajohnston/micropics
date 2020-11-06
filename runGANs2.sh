1cff5a2188da1474154547282dbe1e75304d389d
Pam Johnston <1609098@rgu.ac.uk>
2020-11-06 10:46:22 +0000

    changing GPU

--- runGANs2.sh
+++ runGANs2.sh
@@ -1,5 +1,5 @@
-CUDA_VISIBLE_DEVICES=7 python cycleGANKeras.py -s aphaniz_rd_ori1.npz -d aph_cgan2_rdToOri > aph_cgan2_rdtooriout.txt
-CUDA_VISIBLE_DEVICES=7 python cycleGANKeras.py -s aphaniz_ori_lab1.npz -d aph_cgan2_oriToLab > aph_cgan2_oritolabout.txt
+CUDA_VISIBLE_DEVICES=6 python cycleGANKeras.py -s aphaniz_rd_ori1.npz -d aph_cgan2_rdToOri > aph_cgan2_rdtooriout.txt
+CUDA_VISIBLE_DEVICES=6 python cycleGANKeras.py -s aphaniz_ori_lab1.npz -d aph_cgan2_oriToLab > aph_cgan2_oritolabout.txt
 
-CUDA_VISIBLE_DEVICES=7 python cycleGANKeras.py -s aphaniz_rd_ori2.npz -d aph_cgan2_rdToOri2 > aph_cgan2_rdtooriout2.txt
-CUDA_VISIBLE_DEVICES=7 python cycleGANKeras.py -s aphaniz_ori_lab2.npz -d aph_cgan2_oriToLab2 > aph_cgan2_oritolabout2.txt
+CUDA_VISIBLE_DEVICES=6 python cycleGANKeras.py -s aphaniz_rd_ori2.npz -d aph_cgan2_rdToOri2 > aph_cgan2_rdtooriout2.txt
+CUDA_VISIBLE_DEVICES=6 python cycleGANKeras.py -s aphaniz_ori_lab2.npz -d aph_cgan2_oriToLab2 > aph_cgan2_oritolabout2.txt
